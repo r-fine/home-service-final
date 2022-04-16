ff03afbf9e763dc15fecf4f210f89458462d1ea3
r-fine <saiadul.arfain@tuta.io>
2022-04-17 05:14:49 +0600

    nth commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,61 +1,43 @@
-@section('title', 'Login')
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('../layouts/base')
+@section('title', 'Home')
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+@section('content')
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
+<section class="mt-5 pt-5" style="min-height:84vh">
+    <div class="mx-auto mt-5 pt-5" style="max-width: 425px; margin-top:100px;">
+        <h1 class="text-center text-primary">Sign In</h1>
         <form method="POST" action="{{ route('login') }}">
             @csrf
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
-                    {{ __('Don\'t have an account?') }}
-                </a>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" name="email" class="form-control @error('email') border border-danger @enderror"
+                    placeholder="e.g. name@example.com" value="{{ old('email') }}" required autofocus />
             </div>
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <div class="form-group mt-4">
+                <label for="password">Password</label>
+                <input type="password" name="password"
+                    class="form-control @error('password') border border-danger @enderror"
+                    placeholder="enter your password" required autocomplete="current-password" />
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            <div class="form-group mt-4">
+                <label for="remember_me">
+                    <input type="checkbox" class="form-check-input" name="remember">
+                    <span>Remember me</span>
                 </label>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
+            <div class="row justify-content-xs-center mt-3 mx-auto">
+                <button class="btn btn-secondary" type="submit">Sign In &raquo;</button>
+            </div>
+            <hr>
+            <div class="text-right">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
+                <a class="button" href="{{ route('password.request') }}">Forgot Password?</a>
                 @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
+            </div>
+            <div class="text-left">
+                Don't have an account? <a href="{{ route('register') }}">Sign up here</a>
             </div>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+    </div>
+</section>
+@endsection
